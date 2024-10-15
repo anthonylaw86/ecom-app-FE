547fc907e18dec71671c8e6d3106b3292eed442b
Anthony Law <anthonylaw910@gmail.com>
2024-10-15 13:46:09 -0600

    best seller components

--- FE/src/components/LatestCollection.jsx
+++ FE/src/components/LatestCollection.jsx
@@ -20,7 +20,7 @@ const LatestCollection = () => {
         </p>
       </div>
       {/* Rendering Products */}
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-y-6">
+      <div className=" grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-y-6">
         {latestProducts.map((item, index) => (
           <ProductItem
             key={index}
