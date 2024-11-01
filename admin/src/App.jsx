f8a1a2a0566fc62272c7c76fe1cc6d91eb8d67ef
Anthony Law <anthonylaw910@gmail.com>
2024-11-01 11:46:50 -0600

    login component

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,28 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "./components/NavBar";
 import Sidebar from "./components/Sidebar";
 import { Routes, Route } from "react-router-dom";
 import Add from "./pages/Add";
 import List from "./pages/List";
 import Orders from "./pages/Orders";
+import Login from "./components/Login";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 const App = () => {
+  const [token, setToken] = useState("");
+
   return (
     <div className="bg-gray-50 min-h-screen">
-      <>
-        <NavBar />
-        <hr />
-        <div className="flex w-full">
-          <Sidebar />
-          <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
-            <Routes>
-              <Route path="/add" element={<Add />} />
-              <Route path="/list" element={<List />} />
-              <Route path="/orders" element={<Orders />} />
-            </Routes>
+      <ToastContainer />
+      {token === "" ? (
+        <Login setToken={setToken} />
+      ) : (
+        <>
+          <NavBar />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+              <Routes>
+                <Route path="/add" element={<Add />} />
+                <Route path="/list" element={<List />} />
+                <Route path="/orders" element={<Orders />} />
+              </Routes>
+            </div>
           </div>
-        </div>
-      </>
+        </>
+      )}
     </div>
   );
 };
